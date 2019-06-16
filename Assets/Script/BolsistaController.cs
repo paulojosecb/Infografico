70f58827ce2d7bf5e86a0b33e2e0222da21a18f6
Paulo José <paulojocb@gmail.com>
2019-06-16 19:17:18 -0300

    Commit
    a

---
+++ Assets/Script/BolsistaController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BolsistaController : MonoBehaviour
+{
+    public GameObject bolsistaPrefab;
+    public GameObject cincoBolsistasPrefab;
+    public GameObject dezBolsistasPrefab;
+
+    public void onBolsistaCreate() {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 pos = new Vector3(mousePos.x, mousePos.y, 0);
+        Instantiate(bolsistaPrefab, pos, Quaternion.identity);
+    }
+
+    public void onCincoBolsistasCreate()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 pos = new Vector3(mousePos.x, mousePos.y, 0);
+        Instantiate(cincoBolsistasPrefab, pos, Quaternion.identity);
+    }
+
+    public void onDezBolsistasCreate()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 pos = new Vector3(mousePos.x, mousePos.y, 0);
+        Instantiate(dezBolsistasPrefab, pos, Quaternion.identity);
+    }
+}
---
+++ Assets/Script/DragAndDrop.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private float deltaX;
+    private float deltaY;
+
+    private Vector2 mousePosition;
+    private Rigidbody rigidBody;
+
+    private void Start() {
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.useGravity = false;
+    }
+
+    private void OnMouseDown() {
+        rigidBody.useGravity = false;
+
+        deltaX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x;
+        deltaY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y;
+    }
+
+    private void OnMouseDrag() {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector2(mousePosition.x - deltaX, mousePosition.y - deltaY);
+    }
+
+    private void OnMouseUp() {
+        rigidBody.useGravity = true;
+    }
+
+}
